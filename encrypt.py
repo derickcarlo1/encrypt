adefc653339c133a57d30fcead148b0929faeefa
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-04-20 22:57:33 +0800

    Defining Vigenere ciper encryption/function

--- encrypt.py
+++ encrypt.py
@@ -1,9 +1,22 @@
+import pyfiglet
+
 # Define the Vigenere ciper encryption (function)
+def vigenere_cipher_encrypt(plaintext, keyword):
+    # Translate the keyword into corresponding letter values 0-25
+    keyword_values = [ord(c) - ord('A') for c in keyword]
+
 # Translate the keyword into corresponding letter values 0-25
+
 # Encrypt the plaintext (using Vigenere cipher)
+
 # Show a 'welcome' message
+
 # Get the message and keyword from the user
+
 # Encrypt the given message through Vigenere cipher
+
 # Show the encrypted message
+
 # Ask if the user wants to encrypt another message
+
 # Show a 'goodbye' message
