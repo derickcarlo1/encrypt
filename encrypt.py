d4ffe091e3391aaf3ba9d0c82243134ea98d334c
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-04-20 23:03:12 +0800

    # (Can encrypt uppercase letters ONLY)

--- encrypt.py
+++ encrypt.py
@@ -5,7 +5,21 @@ def vigenere_cipher_encrypt(plaintext, keyword):
     # Translate the keyword into corresponding letter values 0-25
     keyword_values = [ord(c) - ord('A') for c in keyword]
 
-# Encrypt the plaintext (using Vigenere cipher)
+     # Encrypt the plaintext (using Vigenere cipher)
+    ciphertext = ""
+    i = 0
+    for c in plaintext:
+        # (Can encrypt uppercase letters ONLY)
+        if c.isupper():
+            keyword_value = keyword_values[i % len(keyword)]
+            ciphertext +=chr(((ord(c) - ord('A') + keyword_value) % 26) + ord('A'))
+            i += 1
+        else:
+            ciphertext += c
+
+    return ciphertext
+
+while True:
 
 # Show a 'welcome' message
 
