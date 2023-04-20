23c9112cd3024dfb573f8b21c273f49d23267306
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-04-20 22:50:13 +0800

    planning flow/pseudocodes

---
+++ encrypt.py
@@ -0,0 +1,9 @@
+# Define the Vigenere ciper encryption (function)
+# Translate the keyword into corresponding letter values 0-25
+# Encrypt the plaintext (using Vigenere cipher)
+# Show a 'welcome' message
+# Get the message and keyword from the user
+# Encrypt the given message through Vigenere cipher
+# Show the encrypted message
+# Ask if the user wants to encrypt another message
+# Show a 'goodbye' message
